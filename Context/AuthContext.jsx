2fa3732c69fad81eb96832c8227a1251cf8880ba
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-09-18 00:17:36 +0530

    Update AuthContext.jsx

--- Context/AuthContext.jsx
+++ Context/AuthContext.jsx
@@ -1,3 +1,28 @@
+// import { createContext, useContext, useState, useEffect } from "react";
+// import Cookies from "js-cookie";
+// import {jwtDecode} from "jwt-decode";
+// export const AuthContext = createContext();
+// export const useAuthContext = () => {
+// 	return useContext(AuthContext);
+// };
+
+// export const AuthContextProvider = ({ children }) => {
+// 	const [authUser, setAuthUser] = useState(null);
+
+// 	useEffect(() => {
+// 		const jwt = Cookies.get("jwt");
+// 		if (jwt) {
+// 			const user = jwtDecode(jwt);
+// 			setAuthUser(user);
+// 		}
+// 	}, []);
+
+// 	return (
+// 		<AuthContext.Provider value={{ authUser, setAuthUser }}>
+// 			{children}
+// 		</AuthContext.Provider>
+// 	);
+// };
 import { createContext, useContext, useState, useEffect } from "react";
 import Cookies from "js-cookie";
 import {jwtDecode} from "jwt-decode";
@@ -10,7 +35,8 @@ export const AuthContextProvider = ({ children }) => {
 	const [authUser, setAuthUser] = useState(null);
 
 	useEffect(() => {
-		const jwt = Cookies.get("jwt");
+		// const jwt = Cookies.get("jwt");
+		const jwt=localStorage.getItem('jwt');
 		if (jwt) {
 			const user = jwtDecode(jwt);
 			setAuthUser(user);
